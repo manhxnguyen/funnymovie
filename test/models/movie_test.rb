7d0b800780f1db52f2bf34d79047d0a11ccdd3f9
Manh Nguyen <manhxnguyen11@gmail.com>
2021-06-20 00:37:17 +0700

    add unites required field of movie model

--- test/models/movie_test.rb
+++ test/models/movie_test.rb
@@ -4,4 +4,15 @@ class MovieTest < ActiveSupport::TestCase
   # test "the truth" do
   #   assert true
   # end
+  test "Field required" do
+    movie = Movie.new(:user_id => '',
+                      :title => '',
+                      :description => 'Err...',
+                      :link => 'Something')
+
+    assert movie.errors[:title].any?
+    assert movie.errors[:description].any?
+    assert movie.errors[:user_id].any?
+    assert movie.errors[:link].any?
+  end
 end
